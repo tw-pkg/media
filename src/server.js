e5c15d4b8c0ecde5a01a4708e6c7ea3f1bce7d1e
nswon <swnam0729@gmail.com>
2024-05-11 13:12:52 +0900

    worker init 후 팀보이스 연결

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { createServer } from 'http';
 import cors from 'cors';
 import socket from './socket.js';
+import Worker from './worker.js';
 
 const app = express();
 const port = 8080;
@@ -9,7 +10,9 @@ app.use(cors());
 
 const server = createServer(app);
 
-socket(server);
+Worker.init().then(() => {
+  socket(server);
+})
 
 server.listen(port, () => {
   console.log('start media server');
